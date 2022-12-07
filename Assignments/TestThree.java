a1da6dbd70ecf44df418bfc559762a84fc26936d
Pallavi Patil <pallavi.patil@joshsoftware.com>
2022-12-07 12:07:25 +0530

    First 4 Selenium assignments

---
+++ Assignments/TestThree.java
@@ -0,0 +1,70 @@
+package assignments.qatechhub;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class TestThree {
+	WebDriver driver;
+  
+ @Parameters({ "Browser" })
+ @BeforeClass
+ public void openBrowser(String browser) {
+	 
+		  if(browser.equalsIgnoreCase("Chrome"))
+		  {
+			  WebDriverManager.chromedriver().setup();
+			  driver = new ChromeDriver();
+		  }
+		  else if(browser.equalsIgnoreCase("Firefox")) {
+			  WebDriverManager.firefoxdriver().setup();
+			  driver = new FirefoxDriver();
+		  }
+		  else {
+			  System.out.println("Wrong Choice");
+		  }
+		  driver.get("https://flipkart.com");
+		  
+		  //handling lightbox
+		  driver.findElement(By.xpath("//div[@id='container']/following-sibling::div/div/div/button")).click();
+		  
+		  
+  }
+
+  @Test(priority = 0)
+  public void countURL() {
+	  List<WebElement> links = driver.findElements(By.tagName("a"));
+	  System.out.println("Number of links "+links.size());
+  }
+  
+  @Test(priority = 1)
+  public void printURL() {
+	  List<WebElement> links = driver.findElements(By.tagName("a"));
+	  
+	  for(WebElement link : links){
+		  
+			System.out.println("Link Text: "+ link.getText());
+			System.out.println("URL: "+ link.getAttribute("href"));		
+			System.out.println();
+		}
+  }
+ 
+
+  @AfterClass
+  public void afterClass() {
+	  driver.close();
+  }
+
+}
