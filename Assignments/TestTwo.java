cc8b4c12d8f7d65a4610f03d9cf89593c7c49056
Pallavi Patil <pallavi.patil@joshsoftware.com>
2022-12-09 11:39:41 +0530

    Test Description added

--- Assignments/TestTwo.java
+++ Assignments/TestTwo.java
@@ -22,6 +22,7 @@ public class TestTwo {
 	
   @BeforeClass
   public void openBrowser() {
+  	//Open facebook and Verify that the page is redirected to “http://www.facebook.com”, by getting the current URL.
 	  WebDriverManager.chromedriver().setup();
 	  driver = new ChromeDriver();
 	  driver.get("http://www.fb.com");
@@ -37,28 +38,25 @@ public class TestTwo {
 	
   @Test
   public void signUp() {
+  	//Test to see if there is a create account section. Create account
 	  driver.findElement(By.linkText("Create New Account")).click();
 	  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name = \"firstname\"]"))).sendKeys("Pallavi");
 	  driver.findElement(By.name("lastname")).sendKeys("Patil");
 	  driver.findElement(By.name("reg_email__")).sendKeys("abcd@gmail.com");
-      driver.findElement(By.id("password_step_input")).sendKeys("abcd@12");
+     	  driver.findElement(By.id("password_step_input")).sendKeys("abcd@12");
 	  
-      Select day = new Select(driver.findElement(By.id("day")));
+      	  Select day = new Select(driver.findElement(By.id("day")));
 	  Select month = new Select(driver.findElement(By.id("month")));
 	  Select year = new Select(driver.findElement(By.id("year")));
 		
 	  day.selectByVisibleText("11");
-      month.selectByVisibleText("Mar");
+      	  month.selectByVisibleText("Mar");
 	  year.selectByVisibleText("1996");
 	  
 	  driver.findElement(By.xpath("//input[@type=\"radio\" and @value=\"1\"]")).click();
-	  driver.findElement(By.name("websubmit")).click();
-	  
-	 
+	  driver.findElement(By.name("websubmit")).click();	 
   }
-  
- 
 
   @AfterClass
   public void afterClass() {
